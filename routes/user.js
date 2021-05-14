b38e10344b6d99cdd892f0b6bc3dc9c20c602e67
Gabriel Carames <49535753+GabrielCarames@users.noreply.github.com>
2021-05-14 18:57:02 -0300

    registro

--- routes/user.js
+++ routes/user.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/register', function(req, res, next) {
-  res.render('register')
+  res.render('user/register', {layout: false})
 });
 
 module.exports = router;
