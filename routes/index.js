8d2fad00ed02feda645b422e4ef46c28d2819d96
Gabriel Carames <49535753+GabrielCarames@users.noreply.github.com>
2021-05-18 17:30:19 -0300

    xDD la concha det u madre 40 minutos solucionando un error de mierda pense que se me habia bugueado todo el render
    {{}}

--- routes/index.js
+++ routes/index.js
@@ -2,20 +2,14 @@ var express = require('express');
 var router = express.Router();
 const publicationController = require('../controllers/publicationController');
 
-router.get('/', async function(req, res, next) {
+router.get('/', async function(req, res) {
   var user = req.user
   if(!user) {
     res.redirect('/user/login')
   } else {
     const publications = await publicationController.getAllPublications()
-    console.log(publications)
     res.render('index', {publications});
   }
-});
-
-router.get('/index2', async function(req, res, next) {
-  res.render('index2');
-  
 });
 
 module.exports = router;
